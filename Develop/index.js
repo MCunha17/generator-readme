be88146f569ff32c0687157c9fc832d4ace549cb
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-05-04 17:21:16 -0500

    Created function to write README file.

--- Develop/index.js
+++ Develop/index.js
@@ -53,7 +53,15 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+      if (err) {
+        console.error('Error writing file:', err);
+        return;
+      }
+      console.log('README.md was successfully generated');
+    });
+  }
 
 // TODO: Create a function to initialize app
 function init() {}
