97c812d5c9834e471d6388f12b0d630892ac7518
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-05-04 20:08:03 -0500

    Sample ReadMe

--- README.md
+++ README.md
@@ -1,42 +1,8 @@
+# test
+  ![License](https://img.shields.io/badge/license-MIT%20License-blue.svg)
+  ## Description
+  test
+  ## License
 
-        # README Generator
-
-        ## Description
-
-        This command-line application generates a professional README.md file based on the user's inputs.
-
-        ## Table of Contents
-
-        - [Description](#description)
-        - [Installation](#installation)
-        - [Usage](#usage)
-        - [License](#license)
-        - [Contributing](#contributing)
-        - [Tests](#tests)
-        - [Questions](#questions)
-
-        ## Installation
-
-        Ensure that you have Node.js installed on your computer. 2. Clone the repository or download the code. 3. Open a terminal/command prompt and navigate to the project directory. 4. Run 'npm install' 
-
-        ## Usage
-
-        Navigate to the project directory in your terminal/command prompt. 2. Run the command 'node.index.js' 3. Answer the prompts to provide the information about your project.     4. Once completed, a README.md file will generate.
-
-        ## License
-
-        This project is licensed under the MIT License license.
-
-        ## Contributing
-
-        If you would like to contribute to this project, please fork the repository and submit a pull request with your proposed changes.
-
-        ## Tests
-
-        If you would like to contribute by adding tests, please follow the contributing guidelines.
-
-        ## Questions
-
-        My GitHub profile is [GitHub](https://github.com/MCunha17).
-        If you have additional questions, you can reach me at [cunha.maria.theresa@gmail.com](mailto:cunha.maria.theresa@gmail.com).
-        
+This project is licensed under the [MIT License](https://choosealicense.com/licenses/mit-license/) license.
+  
--- index.js
+++ index.js
@@ -1,118 +1,91 @@
 // Inclusion of packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateMarkdown = require('./generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // Array of questions for user input
 const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'Enter a project title',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Enter a project description',
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Enter installation instructions',
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Enter usage instructions',
-    },
-    {
-        type: 'input',
-        name: 'contributing',
-        message: 'Enter contribution guidelines',
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'Enter test instructions',
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'Choose a license',
-        choices: ['Apache License 2.0', 'GNU General Publics License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense', 'None'],
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'Enter your GitHub username',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Enter your email address:',
-    },
-]
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Enter a project title',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter a project description',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Enter installation instructions',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Enter usage instructions',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Enter contribution guidelines',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Enter test instructions',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license',
+    choices: [
+      'Apache License 2.0',
+      'GNU General Publics License v3.0',
+      'MIT License',
+      'BSD 2-Clause "Simplified" License',
+      'BSD 3-Clause "New" or "Revised" License',
+      'Boost Software License 1.0',
+      'Creative Commons Zero v1.0 Universal',
+      'Eclipse Public License 2.0',
+      'GNU General Public License v2.0',
+      'GNU Lesser General Public License v2.1',
+      'Mozilla Public License 2.0',
+      'The Unlicense',
+      'None',
+    ],
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
+];
 
 // Function to write to README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
-      if (err) {
-        console.error('Error creating README.md.', err);
-        return;
-      }
-      console.log('README.md was successfully generated.');
-    });
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error('Error generating README.md file.', err);
+      return;
+    }
+    console.log('The README.md file was successfully generated.');
+  });
 }
 
 // Function to initialize application
 function init() {
-    inquirer.prompt(questions).then((answers) => {
-      const readmeContent = `
-        # ${answers.title}
-
-        ## Description
-
-        ${answers.description}
-
-        ## Table of Contents
-
-        - [Description](#description)
-        - [Installation](#installation)
-        - [Usage](#usage)
-        - [License](#license)
-        - [Contributing](#contributing)
-        - [Tests](#tests)
-        - [Questions](#questions)
-
-        ## Installation
-
-        ${answers.installation}
-
-        ## Usage
-
-        ${answers.usage}
-
-        ## License
-
-        This project is licensed under the ${answers.license} license.
-
-        ## Contributing
-
-        ${answers.contributing}
-
-        ## Tests
-
-        ${answers.tests}
-
-        ## Questions
-
-        My GitHub profile is [GitHub](https://github.com/${answers.github}).
-        If you have additional questions, you can reach me at [${answers.email}](mailto:${answers.email}).
-        `;
+  inquirer.prompt(questions).then((answers) => {
+    const readmeContent = generateMarkdown(answers);
 
     // Function to write README file
-        writeToFile('README.md', readmeContent);
-    });
+    writeToFile('README.md', readmeContent);
+  });
 }
 
 // Function to initialize application
